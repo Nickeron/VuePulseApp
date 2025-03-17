6fbbc1bf85aebe42a309463a2fa10770b5fb35b6
Nick Karamousadakis <nkaramousadakis@outlook.com>
2025-03-17 11:02:04 +0100

    Adds table and dataTable components with Shadcn and Tanstack

--- src/pages/tasks/index.vue
+++ src/pages/tasks/index.vue
@@ -2,6 +2,9 @@
 import { supabase } from '@/lib/supabaseClient'
 import { ref } from 'vue'
 import type { Tables } from '../../../database/types'
+import { h } from 'vue'
+import type { ColumnDef } from '@tanstack/vue-table';
+import DataTable from '@/components/ui/data-table/DataTable.vue';
 
 const tasks = ref<Tables<'tasks'>[] | null>(null);
 (async () => {
@@ -12,16 +15,47 @@ const tasks = ref<Tables<'tasks'>[] | null>(null);
         tasks.value = data
     }
 })()
+
+interface Payment {
+  id: string
+  amount: number
+  status: 'pending' | 'processing' | 'success' | 'failed'
+  email: string
+}
+
+const payments: Payment[] = [
+  {
+    id: '728ed52f',
+    amount: 100,
+    status: 'pending',
+    email: 'm@example.com',
+  },
+  {
+    id: '489e1d42',
+    amount: 125,
+    status: 'processing',
+    email: 'example@gmail.com',
+  },
+  // ...
+]
+
+const columns: ColumnDef<Payment>[] = [
+  {
+    accessorKey: 'amount',
+    header: () => h('div', { class: 'text-right' }, 'Amount'),
+    cell: ({ row }) => {
+      const amount = Number.parseFloat(row.getValue('amount'))
+      const formatted = new Intl.NumberFormat('en-US', {
+        style: 'currency',
+        currency: 'USD',
+      }).format(amount)
+
+      return h('div', { class: 'text-right font-medium' }, formatted)
+    },
+  }
+]
 </script>
 
 <template>
-    <div>
-        <h1>Tasks</h1>
-        <RouterLink to="/">Go Home</RouterLink>
-        <ul>
-            <li v-for="task in tasks" :key="task.id">
-                <RouterLink :to="`/tasks/${task.id}`">{{ task.name }}</RouterLink>
-            </li>
-        </ul>
-    </div>
+    <DataTable :columns="columns" :data="payments"></DataTable>
 </template>
