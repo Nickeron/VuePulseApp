1169aa4727ddc72f548d431d6ffa4ab6de2de06f
Nick Karamousadakis <nkaramousadakis@outlook.com>
2025-03-17 14:20:38 +0100

    Adds unplugin auto import

--- vite.config.ts
+++ vite.config.ts
@@ -1,5 +1,6 @@
 import { fileURLToPath, URL } from 'node:url'
 import VueRouter from 'unplugin-vue-router/vite';
+import AutoImport from 'unplugin-auto-import/vite';
 
 import { defineConfig } from 'vite'
 import autoprefixer from 'autoprefixer'
@@ -14,6 +15,21 @@ export default defineConfig({
     },
   },
   plugins: [
+    AutoImport({
+      include: [
+        /\.[tj]sx?$/, // .ts, .tsx, .js, .jsx
+        /\.vue$/,
+        /\.vue\?vue/, // .vue
+        /\.vue\.[tj]sx?\?vue/, // .vue (vue-loader with experimentalInlineMatchResource enabled)
+        /\.md$/, // .md
+      ],
+      imports: [
+        // presets
+        'vue',
+        'vue-router',],
+      dts: true,
+      viteOptimizeDeps: true
+    }),
     VueRouter(),
     vue({
       template: {
