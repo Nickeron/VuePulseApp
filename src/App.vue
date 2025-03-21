786a627bcb058844fa1cfe59706cb0f883c9dafa
Nick Karamousadakis <nkaramousadakis@outlook.com>
2025-03-21 11:08:39 +0100

    Use onErrorCaptured hook to handle uncaught errors

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,14 @@
 <script setup lang="ts">
 import AppErrorPage from './components/AppError/AppErrorPage.vue';
-import { useErrorStore } from './stores/error';
 
-const { activeError } = storeToRefs(useErrorStore());
+const errorStore = useErrorStore();
+
+onErrorCaptured((error) => { errorStore.setError({ error }) })
 </script>
 
 <template>
   <AuthLayout>
-    <AppErrorPage v-if="activeError" />
+    <AppErrorPage v-if="errorStore.activeError" />
 
     <RouterView v-else v-slot="{ Component, route }">
       <Suspense v-if="Component" :timeout="0">
