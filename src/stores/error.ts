ff8927d1f80b20a919fc218768048831946bf676
Nick Karamousadakis <nkaramousadakis@outlook.com>
2025-03-21 11:03:17 +0100

    Handle native JS errors

--- src/stores/error.ts
+++ src/stores/error.ts
@@ -4,17 +4,17 @@ import type { PostgrestError } from "@supabase/supabase-js";
 export const useErrorStore = defineStore('error-store', () => {
 	const activeError = ref<null | CustomError | ExtendedPostgrestError>(null);
 
-	const setError = ({error, customCode} : {error: string | PostgrestError
-		, customCode: number}) => {
+	const setError = ({error, customCode} : {error: string | PostgrestError | Error
+		, customCode?: number}) => {
 
-			if(typeof error === 'string'){
-				activeError.value = Error(error);
-				activeError.value.customCode = customCode;
+			if(typeof error === 'string' || error instanceof Error){
+				activeError.value = typeof error === 'string' ? Error(error) : error;
+				activeError.value.customCode = customCode || 500;
 				return
 			}
 
 			activeError.value = error;
-			activeError.value.statusCode = customCode;
+			activeError.value.statusCode = customCode || 500;
 	}
 
 	return {
