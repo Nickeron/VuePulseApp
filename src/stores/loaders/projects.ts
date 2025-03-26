8fca30439ffdcc099fe710529637ecbdee74c42a
Nick Karamousadakis <nkaramousadakis@outlook.com>
2025-03-26 13:58:11 +0100

    Stale while revalidate cache

--- src/stores/loaders/projects.ts
+++ src/stores/loaders/projects.ts
@@ -1,17 +1,29 @@
 import { projectsQuery, type Projects } from '@/utils/supaQueries'
 import { useMemoize } from '@vueuse/core'
-
+const projectsKey = 'projects'
 export const useProjectsStore = defineStore('projects-store', () => {
   const projects = ref<Projects | null>(null)
   const loadProjects = useMemoize(async (_key: string) => await projectsQuery)
 
+  const validateCache = () => {
+    if (!projects.value) {
+      projectsQuery.then(({ data }) => {
+        if (JSON.stringify(data) !== JSON.stringify(projects.value)) {
+          loadProjects.delete(projectsKey)
+        }
+      })
+    }
+  }
+
   const getProjects = async () => {
-    const { data, error, status } = await loadProjects('projects')
+    const { data, error, status } = await loadProjects(projectsKey)
     if (error) {
       useErrorStore().setError({ error, customCode: status })
     } else {
       projects.value = data
     }
+
+    validateCache()
   }
 
   return {
