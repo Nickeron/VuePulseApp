bfcaffd1c03cdc8aa2dd52f871e71e6b4faf2391
Nick Karamousadakis <nkaramousadakis@outlook.com>
2025-03-26 14:01:27 +0100

    Update stale data with fresh data

--- src/stores/loaders/projects.ts
+++ src/stores/loaders/projects.ts
@@ -1,15 +1,18 @@
 import { projectsQuery, type Projects } from '@/utils/supaQueries'
 import { useMemoize } from '@vueuse/core'
+
 const projectsKey = 'projects'
+
 export const useProjectsStore = defineStore('projects-store', () => {
   const projects = ref<Projects | null>(null)
   const loadProjects = useMemoize(async (_key: string) => await projectsQuery)
 
   const validateCache = () => {
     if (!projects.value) {
-      projectsQuery.then(({ data }) => {
+      projectsQuery.then(({ data, error }) => {
         if (JSON.stringify(data) !== JSON.stringify(projects.value)) {
           loadProjects.delete(projectsKey)
+          if (!error) projects.value = data
         }
       })
     }
