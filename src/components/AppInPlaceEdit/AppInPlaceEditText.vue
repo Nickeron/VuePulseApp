a2ab1e0ab1587040f4c56b8229c225b62034b639
Nick Karamousadakis <nkaramousadakis@outlook.com>
2025-03-27 13:23:41 +0100

    Manually call blur() for the input, when user presses enter

--- src/components/AppInPlaceEdit/AppInPlaceEditText.vue
+++ src/components/AppInPlaceEdit/AppInPlaceEditText.vue
@@ -6,5 +6,5 @@ defineEmits(['commit'])
 
 <template>
     <input class="w-full p-1 bg-transparent focus:outline-none focus:border-none focus:bg-gray-800 focus:rounded-md"
-        type="text" v-model="value" @blur="$emit('commit')" />
+        type="text" v-model="value" @blur="$emit('commit')" @keyup.enter="($event.target as HTMLInputElement).blur()" />
 </template>
