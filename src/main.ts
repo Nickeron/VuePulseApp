a5d8b40a70d7c6c4d7a6fc6e8ee5c3cb9a297892
Nick Karamousadakis <nkaramousadakis@outlook.com>
2025-03-28 12:28:15 +0100

    Install and setup formkit

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,7 @@
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
+import { plugin } from '@formkit/vue'
+import config from '../formkit.config'
 import './assets/index.css'
 import 'iconify-icon'
 
@@ -10,5 +12,6 @@ const app = createApp(App)
 
 app.use(createPinia())
 app.use(router)
+app.use(plugin, config)
 
 app.mount('#app')
