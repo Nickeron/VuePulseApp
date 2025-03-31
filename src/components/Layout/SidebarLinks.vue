6a5031bf7fb6d4b18cbde8c8c3e315176bfc9753
Nick Karamousadakis <nkaramousadakis@outlook.com>
2025-03-31 14:55:55 +0200

    Add dynamic and manual expansion of sidebar menu

--- src/components/Layout/SidebarLinks.vue
+++ src/components/Layout/SidebarLinks.vue
@@ -16,24 +16,27 @@ const emits = defineEmits<{
 const handleClick = (title: string) => {
   emits('actionClicked', title)
 }
+const { menuOpen } = useMenu()
 </script>
 
 <template>
   <template v-for="link in links" :key="link.title">
-    <RouterLink v-if="link.to" exactActiveClass="text-primary bg-muted" :to="link.to" class="nav-link">
+    <RouterLink v-if="link.to" exactActiveClass="text-primary bg-muted" :to="link.to" class="nav-link"
+      :class="{ 'justify-normal': menuOpen, 'justify-center': !menuOpen }">
       <iconify-icon :icon="link.icon"></iconify-icon>
-      <span class="hidden lg:block text-nowrap">{{ link.title }}</span>
+      <span class="text-nowrap" :class="{ block: menuOpen, hidden: !menuOpen }">{{ link.title }}</span>
     </RouterLink>
 
-    <div v-else class="nav-link cursor-pointer" @click="handleClick(link.title)">
+    <div v-else class="nav-link cursor-pointer" :class="{ 'justify-normal': menuOpen, 'justify-center': !menuOpen }"
+      @click="handleClick(link.title)">
       <iconify-icon :icon="link.icon"></iconify-icon>
-      <span class="hidden lg:block text-nowrap">{{ link.title }}</span>
+      <span class="text-nowrap" :class="{ block: menuOpen, hidden: !menuOpen }">{{ link.title }}</span>
     </div>
   </template>
 </template>
 
 <style scoped>
 .nav-link {
-  @apply flex items-center justify-center gap-3 px-4 py-2 mx-2 transition-colors rounded-lg hover:text-primary lg:justify-normal text-muted-foreground;
+  @apply flex items-center gap-3 px-4 py-2 mx-2 transition-colors rounded-lg hover:text-primary text-muted-foreground;
 }
 </style>
